405810faeab31e22f73541a2484032f5e65b83a0
DiegoCC14 <diego.xmen123@gmail.com>
2022-03-20 01:01:45 -0300

    +Funcionalidad VerArbolGuillotina ArbolGuillotina.py

--- Arbol_Guillotina.py
+++ Arbol_Guillotina.py
@@ -1,35 +1,43 @@
 import random
 
 class Node_Hoja_Guillotina():
-	def __init__( self , dimencionCaja ):
+	def __init__( self , dimencionCaja , padre ):
 		self.dimencionCaja = dimencionCaja
+		self.padre = padre
 
 class Node_CorteGuillotina():
-	def __init__( self , dimencionCaja , corte , Porcentaje_de_Corte ):
+	def __init__( self , dimencionCaja , corte , Porcentaje_de_Corte , padre ):
 		self.dimencionCaja = dimencionCaja
 		self.corte = corte #Puede ser Vertical o Horizontal sobre la caja
 		self.Porcentaje_de_Corte = Porcentaje_de_Corte
-		
+		self.padre = padre
+
 		self.nodo_derecho = None #Puede referir a un Nodo Caja o Nodo Corte Guillotina
 		self.nodo_izquierdo = None #Puede referir a un Nodo Caja o Nodo Corte Guillotina
 
 class Arbol_de_CorteGuillotina():
+	
 	def __init__( self , altura_arbol ):
 		#Los cortes seran aleatorios, rango [0:1]
 		self.raiz = None
 		self.altura = altura_arbol
 	
-	def VerArbolCorteGuillotina( self , nodoActual ):
-		
-		print( '~~~~~~>>>>>>' )
-		print( "Dimenencion: ", nodoActual.dimencionCaja )
-		try:
-			print( "Corte %: ", nodoActual.corte , nodoActual.Porcentaje_de_Corte  )
-			print( '~~~~~~>>>>>>' )
-			self.VerArbolCorteGuillotina( nodoActual.nodo_derecho )
-			self.VerArbolCorteGuillotina( nodoActual.nodo_izquierdo )
-		except:
-			pass
+	def VerArbol_CorteGuillotina( self ):
+		Lista_Arbol = self.representacion_en_forma_de_lista( self.raiz , [] , 0 )
+		contNivel = 0
+		for nivel in Lista_Arbol:
+
+			print( "----------------->>>>> \n Nivel: " , contNivel )
+			for nodo in nivel:
+				if contNivel == 0:
+					print(" Nodo ~> Dim: " + str( nodo.dimencionCaja ) + " - Corte: " + nodo.corte + " - Padre: None " )
+				elif contNivel == self.altura :
+					print(" Hoja ~> Dim:" + str( nodo.dimencionCaja ) + " - Padre: " +str( nodo.padre.dimencionCaja ) )
+				else:
+					print( " Nodo ~> Dim: "+str( nodo.dimencionCaja ) + " - Corte: " + nodo.corte +" - Padre: " +str( nodo.padre.dimencionCaja ) )
+					
+			contNivel += 1
+			print( "----------------->>>>>\n")
 
 	def Arma_arbolGuillotina_aleatoriamente( self , nodoActual , altura_Actual ):
 		
@@ -42,30 +50,53 @@ class Arbol_de_CorteGuillotina():
 			TamCajaIzquierda = ( TamCaja[0] , TamCaja[1] - ( TamCaja[1] * nodoActual.Porcentaje_de_Corte ) )
 		
 		if altura_Actual == 1:
-			nodoActual.nodo_derecho = Node_Hoja_Guillotina( TamCajaDerecha )
-			nodoActual.nodo_izquierdo = Node_Hoja_Guillotina( TamCajaIzquierda )
+			nodoActual.nodo_derecho = Node_Hoja_Guillotina( TamCajaDerecha , nodoActual )
+			nodoActual.nodo_izquierdo = Node_Hoja_Guillotina( TamCajaIzquierda , nodoActual )
 
 		else:
 			corteNodeDerecha = self.corte_aleatorio_vertical_o_horizontal()
 			corteNodeIzquierdo = self.corte_aleatorio_vertical_o_horizontal()
 
-			nodoActual.nodo_derecho = Node_CorteGuillotina( TamCajaDerecha , corteNodeDerecha , random.random() )
-			nodoActual.nodo_izquierdo = Node_CorteGuillotina( TamCajaIzquierda , corteNodeIzquierdo , random.random() )
+			nodoActual.nodo_derecho = Node_CorteGuillotina( TamCajaDerecha , corteNodeDerecha , random.random() , nodoActual )
+			nodoActual.nodo_izquierdo = Node_CorteGuillotina( TamCajaIzquierda , corteNodeIzquierdo , random.random() , nodoActual )
 			
 			self.Arma_arbolGuillotina_aleatoriamente( nodoActual.nodo_derecho , altura_Actual-1 )
 			self.Arma_arbolGuillotina_aleatoriamente( nodoActual.nodo_izquierdo , altura_Actual-1 )
+
+	def representacion_en_forma_de_lista( self , nodoActual , ListaArbol , nivel ):
+
+		try:
+			ListaArbol[nivel].append( nodoActual ) #Si la lista nivel no existe
+		except:
+			ListaArbol.insert( nivel , [ nodoActual ] ) #Insertamos una lista con referencia al nodo actual
+		
+		if nivel != self.altura:	
+			if nodoActual.nodo_derecho != None and nodoActual.nodo_izquierdo != None:
+				ListaArbol = self.representacion_en_forma_de_lista( nodoActual.nodo_derecho , ListaArbol , nivel+1 )
+				ListaArbol = self.representacion_en_forma_de_lista( nodoActual.nodo_izquierdo , ListaArbol , nivel+1 )
+		
+		return ListaArbol
 
 	def corte_aleatorio_vertical_o_horizontal( self ):
 		if random.random() > 0.5:
 			return "H"
 		return "V"
 
+# ------------------->>>
+# -- Configuracion -->>>
+Altura_Arbol = 5
+Dimencion_Caja = (8,5)
+# ------------------->>>
+# ------------------->>>
 
-'''
-ArbolGuillotina = Arbol_de_CorteGuillotina( 3 )
+
+ArbolGuillotina = Arbol_de_CorteGuillotina( Altura_Arbol )
 corte = ArbolGuillotina.corte_aleatorio_vertical_o_horizontal()
-ArbolGuillotina.raiz = Node_CorteGuillotina( (8,5) , corte , random.random() )
+ArbolGuillotina.raiz = Node_CorteGuillotina( Dimencion_Caja , corte , random.random() , None )
 
 ArbolGuillotina.Arma_arbolGuillotina_aleatoriamente( ArbolGuillotina.raiz , ArbolGuillotina.altura )
-ArbolGuillotina.VerArbolCorteGuillotina( ArbolGuillotina.raiz )
-'''
+
+
+#Lista = ArbolGuillotina.representacion_en_forma_de_lista( ArbolGuillotina.raiz , [] , 0 )
+
+ArbolGuillotina.VerArbol_CorteGuillotina()
